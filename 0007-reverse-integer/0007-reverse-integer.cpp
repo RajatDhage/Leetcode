bcd9eae45115d313c4d3a0d5c8523337cd6d50f2
Rajat Dhage <104453729+RajatDhage@users.noreply.github.com>
2024-01-06 11:50:54 +0530

    Time: 5 ms (16.19%), Space: 6.2 MB (75.92%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int temp=0;
+        while(x)
+        {
+            temp = temp*10+x%10;
+            x=x/10;
+        }
+        if(temp>INT_MAX || temp<INT_MIN) return 0;
+        return int(temp);
+    }
+};
