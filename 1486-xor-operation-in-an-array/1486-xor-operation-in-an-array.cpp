6c25e95edf4a56000140298ec3559324d27cb736
Rajat Dhage <104453729+RajatDhage@users.noreply.github.com>
2024-01-24 22:24:49 +0530

    Time: 3 ms (13.87%), Space: 7.4 MB (7.06%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int> nums;
+        for(int i=0;i<n;i++) nums.push_back(start+2*i);
+        
+        int res = 0;
+        for(int i=0;i<n;i++) res ^= nums[i];
+        return res;
+    }
+};
